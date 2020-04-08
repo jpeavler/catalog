ec039c07d743bfbd4439b10f79dbb3392a43bf4d
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-08 11:35:33 -0600

    Implemented Read Articles in Service and DAL

--- data/articles.js
+++ data/articles.js
@@ -7,13 +7,37 @@ const url = process.env.DB_URL;
  
 // Database Name
 const dbName = 'wiki';
+const colName = 'articles';
  
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
+// Database Settings
+const settings = { useUnifiedTopology: true }
+
+const getArticles = () => {
+    const iou = new Promise((resolve, reject) =>{
+    // Use connect method to connect to the server
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err){
+               reject(err);
+            } else{
+                console.log("Connected successfully to server to GET Articles");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.find({}).toArray(function(err, docs) {
+                    if(err){
+                        reject(err);
+                     } else{ 
+                    console.log("Found the following articles");
+                    console.log(docs)
+                    resolve(docs);
+                    client.close();
+                    }
+                });
+            }
+        });
+    });
+    return iou;
+}
+
+module.exports = {
+    getArticles
+}
---
+++ routes/api/articles.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { getArticles } = require('../../data/articles');
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+    try{
+        const data = await getArticles();
+        res.send(data);
+    }catch(err){
+        console.log(err);
+        res.send(500, 'Internal Server Issue, check logs');
+    }
+
+});
+
+module.exports = router;
