752cce0bba09ead5a01a638941739c5a44d5d389
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-09 11:13:07 -0600

    partially implemented patch

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {
      getArticles,
-     addArticle
+     addArticle,
+     updateArticle,
+     deleteArticle
 } = require('../../data/articles');
 
 /* GET users listing. */
@@ -22,11 +24,41 @@ router.post('/', async function( req, res, next) {
         const data = await addArticle(req.body);
         res.send(data);
     } catch(err){
-        if(err.msg){
+        if(err.error){
             res.status(400).send(err);
         }else{
             console.log(err);
             res.status(500).send('Internal Server Issue, check logs'); 
+        }
+    }
+});
+
+/* PATCH article Update */
+router.patch('/:id', async function(req, res, next) {
+    try{
+        const data = await updateArticle(req.params.id, req.body);
+        res.send(data);
+    }catch(err){
+        if(err.error){
+            res.status(400).send(err);
+        }else{
+            console.log(err);
+            res.status(500).send('Internal Server Issue, check logs');
+        }
+    }
+})
+
+/* DELETE article Deletion */
+router.delete('/:id', async function(req, res, next){
+    try{
+        const data = await deleteArticle(req.params.id);
+        res.send(data);
+    }catch(err){
+        if(err.error){
+            res.status(400).send(err);
+        }else{
+            console.log(err);
+            res.status(500).send('Internal Server Issue, check log');
         }
     }
 });
