6ff8006aa5360db9aa0ca315c0d0c6ef02ff2d4b
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-08 22:04:52 -0600

    display articles in front end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+const {
+  getArticles,
+  addArticle
+} = require('../data/articles');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/articles', async function(req, res, next) {
+  try{
+    const data = await getArticles();
+    res.render('articles', {title: 'Articles', data: data});
+}catch(err){
+    console.log(err);
+    res.send(500, 'Internal Server Issue, check logs');
+}
+
+})
 module.exports = router;
