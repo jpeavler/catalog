e18ce6aa47b9220db25c51ff47d972bd4f1db55a
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-09 16:53:15 -0600

    fixed typo of author key while Creating new article

--- public/javascripts/articles.js
+++ public/javascripts/articles.js
@@ -1,4 +1,26 @@
 const deleteButtons = document.getElementsByClassName('delete');
+const titleInput = document.getElementById('title');
+const arthorInput = document.getElementById('author');
+const linkInput = document.getElementById('link');
+const descInput = document.getElementById('desc');
+
+const submitHandler = () => {
+    console.log("Attempt to submit");
+    let subData = [{
+        'title': titleInput.value,
+        'author': arthorInput.value,
+        'link' : linkInput.value,
+        'desc' : descInput.value
+    }]
+    fetch(`/api/articles`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(subData)
+    }).then(() => window.location.replace('/articles'));
+}
+submit.addEventListener('click', submitHandler);
 
 const deleteHandler = (event) => {
     console.log("ID: ", event.target.id);
